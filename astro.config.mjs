f48d0dac1711deebaf662f665b553f54124ed1ef
Fred Hsu <fredlhsu@gmail.com>
2026-01-16 19:26:02 -0800

    added rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://fredhsu-tutorial-blog.netlify.app/",
+});
